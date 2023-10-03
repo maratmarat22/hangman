46f8fbd1a8c98ba4ec1439192d5f350d911021e2
Rustem <dzhedz@gmail.com>
2023-10-03 15:57:20 +0300

    по русски

---
+++ виселица/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace виселица
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string word = "виселица";
+
+
+            char[] hiddenword = new char[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                hiddenword[i] = '_';
+            }
+
+            Console.WriteLine("{0}", string.Join(" ", hiddenword));
+
+            int mistakes = 0;
+
+            char letter = Console.ReadKey().KeyChar;
+
+            while (mistakes < 6)
+            {
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (letter == word[i])
+                    {
+                        hiddenword[i] = letter;
+                    }
+                }
+
+                Console.WriteLine("\n{0}", string.Join(" ", hiddenword));
+                letter = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
